51dfa0e0e337265d96b44e65547a2e9f80f22182
TomasRighetti <trighetti@frba.utn.edu.ar>
2022-12-02 01:05:05 -0300

    Added files for day 1. Not Completed

---
+++ day_01/solution.py
@@ -0,0 +1,22 @@
+# 1. Create an elfs dictionary to store each one with it's value
+elfs = {}
+
+# 2. Define the counter for the number of elfs
+elf = 1
+
+# 3. Open the elfs_calories file and read elf-by-elf storing their calories inside the dictionary
+with open("elfs_calories.txt", "r") as elfs_calories:
+	for index, calorie in enumerate(elfs_calories):
+		if calorie == '\n':
+			elfs[index] = 0
+	
+
+
+
+
+print(elfs)
+
+
+
+elves_calories = []
+
